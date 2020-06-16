d47a70a75d853017542f2ef6a0c78037905923bf
Toan Nguyen <ztoan11120@gmail.com>
2020-06-16 21:37:06 +0700

    update

--- BTLQA/src/test/TestPaymenHistoryDao.java
+++ BTLQA/src/test/TestPaymenHistoryDao.java
@@ -7,39 +7,32 @@ import javax.naming.NamingException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import dao.InsuranceDao;
 import dao.UserPaymentHistoryDao;
-import dao.impl.InsuranceDaoImpl;
 import dao.impl.UserPaymentHistoryDaoImpl;
-import model.Insurance;
 import model.Search;
 import model.UserPaymentHistory;
 public class TestPaymenHistoryDao {
 
 	private UserPaymentHistoryDao uphDao = new UserPaymentHistoryDaoImpl();
-	private InsuranceDao insuranceDao = new InsuranceDaoImpl();
 	private List<UserPaymentHistory> uphList;
-	private List<Insurance> insurances;
 	private Search search;
 	
 
 	@Test
-	public void test1() throws NamingException {
+	public void test1() {
 		// init
-		String idProvince = "36";
+		String name = "Nguyễn Quang Toàn";
 		search = new Search();
-		search.setIdProvince(idProvince);
+		search.setName(name);
 
 		// get result by dao function
 		uphList = uphDao.search(search);
-		insurances = insuranceDao.search(search);
-
 		// test
 		Assert.assertNotNull(uphList);
-		Assert.assertEquals(2, uphList.size());
+		Assert.assertEquals(1, uphList.size());
 		for (UserPaymentHistory history : uphList) {
-			Assert.assertEquals(idProvince,
-					history.getInsurance().getUser().getVillage().getDistrict().getProvince().getId());
+			Assert.assertEquals(name,
+					history.getInsurance().getUser().getName());
 		}
 	}
 }
